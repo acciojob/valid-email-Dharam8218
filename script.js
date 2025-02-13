f20f2e0d2b82dfd2e658ed6f77a56fc65d9cd817
Dharam8218 <101041058+Dharam8218@users.noreply.github.com>
2025-02-13 13:53:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function validEmail(str) {
   //your JS code here.
+	if(str.length == 0){
+		return false;
+	}
+	if(str.indexOf("@")!= -1 && str.indexOf("@")!=0 && str.indexOf(".")!=-1 && str.indexOf("@com")==-1 && str.charAt(str.length-1)!=".")  {
+		return true;
+	}
+
+	return false;
 }
 
 // Do not change the code below.
+
+
+
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
